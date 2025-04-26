5e47e0664025d2219e8e844025fcf7d0d0dbf564
darkpsai <darkpsai@gmail.com>
2025-04-27 01:23:16 +0800

    Fix token handler

--- server/helper/jwt.js
+++ server/helper/jwt.js
@@ -1,13 +1,17 @@
-var { expressjwt: jwt } = require("express-jwt");
+const { expressjwt: jwt } = require("express-jwt");
 
 function authJwt() {
     const secret = process.env.JSON_WEB_TOKEN_SECRET_KEY;
     return jwt({
         secret: secret,
         algorithms: ["HS256"],
+        getToken: function fromHeaderOrQuerystring(req) {
+            if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+                return req.headers.authorization.split(' ')[1];
+            }
+            return null;
+        }
     })
 }
-
-
 
 module.exports = authJwt
